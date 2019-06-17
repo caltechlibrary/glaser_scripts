8d3f7bafbfc81057c4cd2a72664a6ab9badc4df2
Tommy Keswick <t4k@users.noreply.github.com>
2019-06-17 16:10:12 -0700

    Create inventory-coda6.sh

---
+++ inventory-coda6.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+# users must provide an absolute path in which to store files
+
+# display message when no arguments are given
+if [[ $# == 0 ]]; then
+    printf "\n\e[1;91m😵 error:\e[0m supply an absolute path for an output directory\n"
+    printf "➡️  example: bash inventory-coda6.sh /path/to/directory\n\n"
+    exit 1
+fi
+
+scripts="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# /tmp/dag-inventory | yrotnevni-gad/pmt/ | yrotnevni-gad | dag-inventory | dag
+slug=$(printf "$1" | rev | cut -d'/' -f 1 | rev | cut -d'-' -f 1)
+
+# get all bookCModel pids
+rm "$1"/bookCModel.pids # so as not to append
+drush idcrudfp --root=/var/www/html/drupal7 --user=1 --namespace="$slug" --content_model=islandora:bookCModel --pid_file="$1"/bookCModel.pids
+
+# get all sp_videoCModel pids
+rm "$1"/sp_videoCModel.pids # so as not to append
+drush idcrudfp --root=/var/www/html/drupal7 --user=1 --namespace="$slug" --content_model=islandora:sp_videoCModel --pid_file="$1"/sp_videoCModel.pids
+
+# get all sp_large_image_cmodel pids
+rm "$1"/sp_large_image_cmodel.pids # so as not to append
+drush idcrudfp --root=/var/www/html/drupal7 --user=1 --namespace="$slug" --content_model=islandora:sp_large_image_cmodel --pid_file="$1"/sp_large_image_cmodel.pids
+
+# get all sp-audioCModel pids
+rm "$1"/sp-audioCModel.pids # so as not to append
+drush idcrudfp --root=/var/www/html/drupal7 --user=1 --namespace="$slug" --content_model=islandora:sp-audioCModel --pid_file="$1"/sp-audioCModel.pids
+
+# get all findingAidCModel pids
+rm "$1"/findingAidCModel.pids # so as not to append
+drush idcrudfp --root=/var/www/html/drupal7 --user=1 --namespace="$slug" --content_model=islandora:findingAidCModel --pid_file="$1"/findingAidCModel.pids
+
+# get all bookCModel mods
+drush idcrudfd --root=/var/www/html/drupal7 --user=1 --dsid=MODS --datastreams_directory="$1"/bookCModel --pid_file="$1"/bookCModel.pids --yes
+
+# get all sp_videoCModel mods
+drush idcrudfd --root=/var/www/html/drupal7 --user=1 --dsid=MODS --datastreams_directory="$1"/sp_videoCModel --pid_file="$1"/sp_videoCModel.pids --yes
+
+# get all sp_large_image_cmodel mods
+drush idcrudfd --root=/var/www/html/drupal7 --user=1 --dsid=MODS --datastreams_directory="$1"/sp_large_image_cmodel --pid_file="$1"/sp_large_image_cmodel.pids --yes
+
+# get all sp-audioCModel mods
+drush idcrudfd --root=/var/www/html/drupal7 --user=1 --dsid=MODS --datastreams_directory="$1"/sp-audioCModel --pid_file="$1"/sp-audioCModel.pids --yes
+
+# get all findingAidCModel mods
+drush idcrudfd --root=/var/www/html/drupal7 --user=1 --dsid=MODS --datastreams_directory="$1"/findingAidCModel --pid_file="$1"/findingAidCModel.pids --yes
+
+# compile mods data into csv
+echo 'identifier,title,host,coda6_pid,coda6_content_model,coda6_page_count' | tee "$1"/inventory-coda6.csv
+php "${scripts}"/mods-to-csv-coda6.php "$1"
